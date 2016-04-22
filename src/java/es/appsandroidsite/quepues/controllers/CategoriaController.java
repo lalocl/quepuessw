29a73db5775a0abf99dd70aa825bd1a275325b0c
laura <laura>
2016-04-22 12:49:27 +0200

    Conexión con el servidor OK

--- src/java/es/appsandroidsite/quepues/controllers/CategoriaController.java
+++ src/java/es/appsandroidsite/quepues/controllers/CategoriaController.java
@@ -5,10 +5,42 @@
  */
 package es.appsandroidsite.quepues.controllers;
 
+import es.appsandroidsite.quepues.json.JsonTransformer;
+import es.appsandroidsite.quepues.modelo.Categoria;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author laura
  */
+@Controller
 public class CategoriaController {
     
+  /*   @Autowired
+    private JsonTransformer jsonTransformer;
+    */
+     
+     @RequestMapping(value = {"/Categoria"})
+    public void prueba(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) 
+            throws IOException {
+        httpServletResponse.getWriter().println("Probando...");
+        
+          /*  
+            Categoria c = new Categoria("Marketing","M");
+            String jsonCategoria = jsonTransformer.toJson(c);
+            httpServletResponse.getWriter().println(jsonCategoria);*/
+            
+        
+        
+      
+     
 }
+    
+}
+
+
---
+++ src/java/es/appsandroidsite/quepues/modelo/Categoria.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.appsandroidsite.quepues.modelo;
+
+/**
+ *
+ * @author laura
+ */
+public class Categoria {
+    
+    String nombre;
+    String codigo;
+
+    public Categoria(String nombre, String codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+    
+    
+}
